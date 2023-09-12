132dfc64c7526055b9f6a478aa7c2c3ddbb9e802
EngineX-GB <uk.engine.x@gmail.com>
2023-09-11 19:27:40 -0500

    Updated chromedriver details

--- src/main/java/com/omega/discovery/adapters/impl/ChromeDevToolsAdapter.java
+++ src/main/java/com/omega/discovery/adapters/impl/ChromeDevToolsAdapter.java
@@ -4,13 +4,18 @@ import java.io.File;
 import java.util.HashSet;
 import java.util.Optional;
 import java.util.Set;
+import java.util.concurrent.TimeUnit;
 
 import com.omega.discovery.dto.AdapterResponse;
 import com.omega.discovery.dto.Result;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.devtools.network.Network;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -31,8 +36,8 @@ public class ChromeDevToolsAdapter implements NetworkMediaAdapter{
 		final Result result = new Result();
 		System.setProperty("webdriver.chrome.driver", config.getChromeDriverPath());
 		ChromeOptions options = new ChromeOptions();
-		//options.addArguments("--headless");
-		options.setBinary(new File(config.getChromeExecutablePath()));
+		//options.addArguments("--headless"); //TODO: undo this
+		//options.setBinary(new File(config.getChromeExecutablePath()));
 		ChromeDriver chromeDriver = new ChromeDriver(options);
 		
 		DevTools chromeDevTools = chromeDriver.getDevTools();
@@ -45,27 +50,58 @@ public class ChromeDevToolsAdapter implements NetworkMediaAdapter{
 //        chromeDevTools.send(Network.setBlockedURLs(ImmutableList.of("*.css", "*.png")));
 
         //add event listener to verify that css and png are blocked
-        chromeDevTools.addListener(Network.requestWillBeSent(), responseReceived -> {
-			if (responseReceived.getRequest().getUrl().contains("\\.ts")) {
-				System.out.println("OUT>> TS FILE");
-			}
-        	if (config.getMappingEntry().getMatchType().equals("contains")) {
-        		if (responseReceived.getRequest().getUrl().contains(config.getMappingEntry().getRegex())) {
-        			detectedLinks.add(responseReceived.getRequest().getUrl());
-            	}
-        	}
-        });
+//        chromeDevTools.addListener(Network.requestWillBeSent(), responseReceived -> {
+//			if (responseReceived.getRequest().getUrl().contains("\\.ts")) {
+//				System.out.println("OUT>> TS FILE");
+//			}
+//        	if (config.getMappingEntry().getMatchType().equals("contains")) {
+//        		if (responseReceived.getRequest().getUrl().contains(config.getMappingEntry().getRegex())) {
+//        			detectedLinks.add(responseReceived.getRequest().getUrl());
+//            	}
+//        	}
+//        });
 
 
 
 		chromeDriver.get(url);
-		if (config.getMappingEntry().getXpathClick() != null) {
-			chromeDriver.findElementByXPath(config.getMappingEntry().getXpathClick()).click();
-		}
-		if (config.getMappingEntry().getElementById() != null) {
-			chromeDriver.findElementById(config.getMappingEntry().getElementById()).click();
+		chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		WebDriverWait wait = new WebDriverWait(chromeDriver, 40);
+//		if (config.getMappingEntry().getXpathClick() != null) {
+//			chromeDriver.findElementByXPath(config.getMappingEntry().getXpathClick()).click();
+//		}
+//		if (config.getMappingEntry().getElementById() != null) {
+//			chromeDriver.findElementById(config.getMappingEntry().getElementById()).click();
+//		}
+
+		wait.until(ExpectedConditions.visibilityOf(chromeDriver.findElementByXPath("/html/body/div[4]/div/div/button")));
+		if (chromeDriver.findElementByXPath("/html/body/div[4]/div/div/button") != null) {
+			chromeDriver.findElementByXPath("/html/body/div[4]/div/div/button").click();
 		}
 
+		wait.until(ExpectedConditions.visibilityOf(chromeDriver.findElementByXPath("/html/body/div[4]/div/button[1]")));
+		if (chromeDriver.findElementByXPath("/html/body/div[4]/div/button[1]") != null) {
+			chromeDriver.findElementByXPath("/html/body/div[4]/div/button[1]").click();
+		}
+
+		if (chromeDriver.findElementById("player") != null) {
+			chromeDriver.findElementById("player").click();
+		}
+
+		chromeDevTools.addListener(Network.requestWillBeSent(), responseReceived -> {
+			if (responseReceived.getRequest().getUrl().contains("\\.ts")) {
+				System.out.println("OUT>> TS FILE");
+			}
+			if (config.getMappingEntry().getMatchType().equals("contains")) {
+				if (responseReceived.getRequest().getUrl().contains(config.getMappingEntry().getRegex())) {
+					detectedLinks.add(responseReceived.getRequest().getUrl());
+				}
+			}
+		});
+		try {
+			Thread.sleep(10000);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
 		chromeDevTools.close();
 		chromeDriver.quit();
 		LOGGER.info("Status: END");
